8d45d336ea1c8dc9659122f1afc450bb55fa7c0e
fblondel <fblondel@FBLONDEL-SURFAC>
2022-12-01 20:59:01 -0600

    Add project files.

---
+++ Days/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022.Days
+{
+    internal static class Day1Part1
+    {
+        public static int Run()
+        {
+            int max = 0, calories = 0;
+            foreach(var line in File.ReadAllLines("Inputs/Day1.txt"))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    max = calories > max ? calories : max;
+                    calories = 0;
+                    continue;
+                }
+
+                calories += int.Parse(line);
+            }
+
+            return max;
+        }
+    }
+
+    internal static class Day1Part2
+    {
+        public static int Run()
+        {
+            int calories = 0;
+            List<int> elves = new();
+            foreach (var line in File.ReadAllLines("Inputs/Day1.txt"))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    elves.Add(calories);
+                    calories = 0;
+                    continue;
+                }
+
+                calories += int.Parse(line);
+            }
+
+            return elves.OrderByDescending(e => e).Take(3).Sum();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using AdventOfCode2022.Days;
+using static System.Console;
+
+WriteLine("+ * = * + = * = +| Advent of Code 2022 |+ = * = + * = * +");
+WriteLine("                 =>+-+>   Day 1   <+-+<=                 ");
+WriteLine("        >-=>>+-+>   Calorie Counting   <+-+<<=-<         ");
+WriteLine($"+> Part 1: The elf carrying the most calories carries {Day1Part1.Run()} calories");
+WriteLine($"+> Part 2: The first 3 elves with the most calories are carrying {Day1Part2.Run()} calories");
