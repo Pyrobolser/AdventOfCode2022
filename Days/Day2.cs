f58d04eceb88599851a8aba77982e6d72036c587
Pyrobolser <francois.blondel@outlook.com>
2022-12-02 21:55:03 -0600

    Day 2!

---
+++ Days/Day2.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+
+namespace AdventOfCode2022.Days
+{
+    public static class Day2Part1
+    {
+        public static int Run()
+        {
+            var score = 0;
+            foreach(var line in File.ReadLines("Inputs/Day2.txt"))
+            {
+                score += GetScore(line);
+            }
+
+            return score;
+        }
+
+        private static int GetScore(string round) => round switch
+        {
+            "A X" => 4,
+            "A Y" => 8,
+            "A Z" => 3,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 7,
+            "C Y" => 2,
+            "C Z" => 6,
+            _ => throw new System.ArgumentOutOfRangeException(),
+        };
+    }
+
+    public static class Day2Part2
+    {
+        public static int Run()
+        {
+            var score = 0;
+            foreach (var line in File.ReadLines("Inputs/Day2.txt"))
+            {
+                score += GetScore(line);
+            }
+
+            return score;
+        }
+
+        private static int GetScore(string round) => round switch
+        {
+            "A X" => 3,
+            "A Y" => 4,
+            "A Z" => 8,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 2,
+            "C Y" => 6,
+            "C Z" => 7,
+            _ => throw new System.ArgumentOutOfRangeException(),
+        };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,3 +6,7 @@ WriteLine("                 =>+-+>   Day 1   <+-+<=                 ");
 WriteLine("        >-=>>+-+>   Calorie Counting   <+-+<<=-<         ");
 WriteLine($"+> Part 1: The elf carrying the most calories carries {Day1Part1.Run()} calories");
 WriteLine($"+> Part 2: The first 3 elves with the most calories are carrying {Day1Part2.Run()} calories");
+WriteLine("                 =>+-+>   Day 2   <+-+<=                 ");
+WriteLine("       >-=>>+-+>   Rock Paper Scissors   <+-+<<=-<       ");
+WriteLine($"+> Part 1: By following the strategy guide, the score will be {Day2Part1.Run()}");
+WriteLine($"+> Part 2: With the whole explanation, the score will be {Day2Part2.Run()}");
