6e5b13cf942d2f89e8f6fb824201fdba58d9abce
Pyrobolser <francois.blondel@outlook.com>
2022-12-04 17:18:57 -0600

    Day 3!

---
+++ Days/Day3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022.Days
+{
+    public static class Day3Part1
+    {
+        public static int Run()
+        {
+            var priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            return File.ReadAllLines("Inputs/Day3.txt").Select(l => Array.IndexOf(priorities, l[..(l.Length / 2)].Intersect(l[(l.Length / 2)..]).Single()) + 1).Sum();
+        }
+    }
+
+    public static class Day3Part2
+    {
+        public static int Run()
+        {
+            var priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            var rucksacks = File.ReadAllLines("Inputs/Day3.txt");
+            var total = 0;
+            for(int i = 0; i < rucksacks.Length; i += 3)
+            {
+                total += Array.IndexOf(priorities, rucksacks[i].Intersect(rucksacks[i + 1]).Intersect(rucksacks[i + 2]).Single()) + 1;
+            }
+
+            return total;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 using static System.Console;
 
 WriteLine("+ * = * + = * = +| Advent of Code 2022 |+ = * = + * = * +");
-WriteLine("                 =>+-+>   Day 1   <+-+<=                 ");
-WriteLine("        >-=>>+-+>   Calorie Counting   <+-+<<=-<         ");
-WriteLine($"+> Part 1: The elf carrying the most calories carries {Day1Part1.Run()} calories");
-WriteLine($"+> Part 2: The first 3 elves with the most calories are carrying {Day1Part2.Run()} calories");
-WriteLine("                 =>+-+>   Day 2   <+-+<=                 ");
-WriteLine("       >-=>>+-+>   Rock Paper Scissors   <+-+<<=-<       ");
-WriteLine($"+> Part 1: By following the strategy guide, the score will be {Day2Part1.Run()}");
-WriteLine($"+> Part 2: With the whole explanation, the score will be {Day2Part2.Run()}");
+//WriteLine("                 =>+-+>   Day 1   <+-+<=                 ");
+//WriteLine("        >-=>>+-+>   Calorie Counting   <+-+<<=-<         ");
+//WriteLine($"+> Part 1: The elf carrying the most calories carries {Day1Part1.Run()} calories");
+//WriteLine($"+> Part 2: The first 3 elves with the most calories are carrying {Day1Part2.Run()} calories");
+//WriteLine("                 =>+-+>   Day 2   <+-+<=                 ");
+//WriteLine("       >-=>>+-+>   Rock Paper Scissors   <+-+<<=-<       ");
+//WriteLine($"+> Part 1: By following the strategy guide, the score will be {Day2Part1.Run()}");
+//WriteLine($"+> Part 2: With the whole explanation, the score will be {Day2Part2.Run()}");
+WriteLine("                 =>+-+>   Day 3   <+-+<=                 ");
+WriteLine("     >-=>>+-+>   Rucksack Reorganization   <+-+<<=-<     ");
+WriteLine($"+> Part 1: The sum of the priorities of those item types is {Day3Part1.Run()}");
+WriteLine($"+> Part 2: The sum of the priorities of those item types is {Day3Part2.Run()}");
