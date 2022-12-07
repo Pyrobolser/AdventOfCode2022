5e2843f4e296bb17f615b10658cd9d51762766b0
Pyrobolser <francois.blondel@outlook.com>
2022-12-06 18:31:24 -0600

    Day 6!

---
+++ Days/Day6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022.Days
+{
+    public static class Day6
+    {
+        public static int Run(int length)
+        {
+            var data = File.ReadAllText("Inputs/Day6.txt").AsSpan();
+            var index = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data.Slice(i, length).ToArray().ToHashSet().Count == length)
+                {
+                    index = i + length;
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
